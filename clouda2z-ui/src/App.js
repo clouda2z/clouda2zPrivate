497da7bf565adaf037111808f5b4e74452e36e77
Krishan <krishan2aws@gmail.com>
2020-05-12 22:21:48 -0500

    CloudA2Z Initial commit

--- clouda2z-ui/src/App.js
+++ clouda2z-ui/src/App.js
@@ -1,23 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Home from './Components/Home/Home';
+import Header from './Components/Header/Header';
+import Timer from './Components/Header/Timer';
 
 function App() {
   return (
     <div className="App">
+      <Timer></Timer>
+      <Header appName="CloudA2Z"></Header>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <Home name="Krishan"/>
       </header>
     </div>
   );
---
+++ clouda2z-ui/src/Components/Home/Home.js
@@ -0,0 +1,8 @@
+import React from 'react';
+function Home(props)
+{
+    return (
+        <h1> Welcome to Cloud A 2Z {props.name} </h1>
+    )
+}
+export default Home;
